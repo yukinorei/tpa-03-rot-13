4bf8941b31761cc6ee34ffa10a0380fac1fc63ec
Kan Adachi <obber@users.noreply.github.com>
2018-09-23 13:59:02 -0700

    Update README.md

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,7 @@ const handleBtnConvert = function() {
 const initPage = function() {
   const form = document.querySelector('#container .form-convert');
   // see https://developer.mozilla.org/ja/docs/Web/API/Event/preventDefault
-  form.addEventListener('submit', (evt) => evt.preventDefault());
+  form.addEventListener('submit', evt => evt.preventDefault());
 
   document.querySelector('.btn-convert')
     .addEventListener('click', handleBtnConvert);
