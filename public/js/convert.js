b207b56e71c513b072e26f0b1bbb775556f717af
yukinorei <>
2018-09-24 21:42:58 +0900

    修正コミット

--- public/js/convert.js
+++ public/js/convert.js
@@ -4,6 +4,9 @@ const codeOutput = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'.split(
 const convert = ch => (
   stringInput.indexOf(ch) > -1 ? stringInput[codeOutput.indexOf(ch)] : ch);
 
+// 文章を配列に分割して、その1つ1つにconvert関数を使い、その結果を結合する
+const convertRot13 = sourceText => sourceText.split('').map(convert).join('');
+
 export {
-  convert,
+  convertRot13,
 };
